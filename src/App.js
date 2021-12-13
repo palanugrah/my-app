6cbd9cf01083666225e4437304ed870d2a188299
anugrah pal <anugrahpal@gmail.com>
2021-12-13 18:46:53 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+import React ,{Component} from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters : [],
+      serachField : ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = (e) => {
+  this.setState({serachField : e.target.value})
+  };
+
+  
+  render(){
+    const {monsters,serachField}= this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(serachField.toLowerCase())
+    );
+    return(
+    <div className="App">
+      <SearchBox placeholder ='Search Monsters' handleChange={this.handleChange}/>
+      <CardList monsters = {filteredMonsters} />
+    </div>
+    )}
+}
+
+export default App;
